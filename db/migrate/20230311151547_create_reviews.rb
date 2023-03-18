d0b84aac0a591cc9ac05ac07dc05d0a47105ed9e
obm48er <red1970xz@outlook.jp>
2023-03-18 10:02:00 +0000

    レビュー訂正

--- db/migrate/20230311151547_create_reviews.rb
+++ db/migrate/20230311151547_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user,type: :integer, null: false, foreign_key: true
       t.references :ship, null: false, foreign_key: true
       t.string :title, null: false
       t.string :body, null: false

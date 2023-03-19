80b0274ef1ea4cb7e9dd6702f62a20c8849be10d
obm48er <red1970xz@outlook.jp>
2023-03-19 05:03:36 +0000

    レビュー4

--- db/migrate/20230311151361_create_ships.rb
+++ db/migrate/20230311151361_create_ships.rb
@@ -1,11 +1,14 @@
 class CreateShips < ActiveRecord::Migration[6.1]
   def change
     create_table :ships do |t|
-      t.references :city, null: false, foreign_key: true
+      #t.references :city, null: false, foreign_key: true
+      t.bigint :city_id
       t.string :name, null: false
       t.string :body, null: false
       t.string :phone_number,null: false
       t.timestamps
     end
+    
+    add_foreign_key :ships, :cities, column: :city_id
   end
 end

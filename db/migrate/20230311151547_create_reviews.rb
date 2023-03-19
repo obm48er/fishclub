2a87d2d58c5f59c15dfd9573df5249822e02bf99
obm48er <red1970xz@outlook.jp>
2023-03-19 04:47:50 +0000

    レビュー3

--- db/migrate/20230311151547_create_reviews.rb
+++ db/migrate/20230311151547_create_reviews.rb
@@ -1,12 +1,17 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.references :user,type: :bigint, null: false, foreign_key: true
-      t.references :ship, null: false, foreign_key: true
+      #t.references :user,type: :bigint, null: false, foreign_key: true
+      #t.references :ship, null: false, foreign_key: true
+      t.bigint :user_id, null: false,
+      t.bigint :ship_id, null: false,
       t.string :title, null: false
       t.string :body, null: false
       t.integer :star,null: false, default: "0"
       t.timestamps
     end
+    
+    add_foreign_key :reviews, :users, column: :user_id
+    add_foreign_key :reviews, :ships, column: :ship_id
   end
 end
